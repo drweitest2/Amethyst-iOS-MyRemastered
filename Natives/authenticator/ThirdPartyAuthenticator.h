6a0d3900ea70d994aa7f416e98e12392b4b8568d
drweitest2 <13644024782@163.com>
2025-09-13 08:33:38 +0800

    Delete Natives/authenticator/ThirdPartyAuthenticator.h

--- Natives/authenticator/ThirdPartyAuthenticator.h
+++
@@ -1,8 +0,0 @@
-#import "BaseAuthenticator.h"
-
-@interface ThirdPartyAuthenticator : BaseAuthenticator
-
-// Expose designated initializer to callers to avoid "no visible @interface" errors.
-- (id)initWithData:(NSMutableDictionary *)data;
-
-@end
